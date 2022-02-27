c08e41e351b7e6595f28412e9e0518ab408e9b28
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-27 17:26:49 +1100

    feat: build hero

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,16 @@
 import Header from '$components/Header'
+import Hero from '$components/Hero'
 
 export default function Home() {
   return (
     <>
       <Header />
+
+      <div className="mx-auto max-w-7xl">
+        <Hero />
+
+        {/* Posts */}
+      </div>
     </>
   )
 }
